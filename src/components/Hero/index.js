febf10d4fcf5b7b13650ac0a0d1466eebf8f8855
danielkuzminski <dankuzmina@gmail.com>
2021-02-06 16:31:05 +0100

    create blog, main components

---
+++ src/components/Hero/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+/**
+* @author
+* @function Hero
+**/
+
+const Hero = (props) => {
+  return(
+    <div></div>
+   )
+
+ }
+
+export default Hero
